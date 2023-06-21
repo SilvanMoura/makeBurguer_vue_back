89756789b6f3095e387a1ae5418831b31ec708cb
Silvan Moura <silvancortesmoura@outlook.com>
2023-06-21 18:10:09 -0300

    Creating route and method to collect the ingredients

--- app/Http/Controllers/BurguerController.php
+++ app/Http/Controllers/BurguerController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class BurguerController extends Controller
 {
-    //
+    public function burguerIngredients()
+    {
+        $tabela = 'ingredients';
+        
+        $dados = DB::table($tabela)->get();
+
+        return response()->json($dados);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BurguerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [ AuthController::class, 'login' ]);
 Route::post('/register', [ AuthController::class, 'register' ]);
+
+Route::get('/burguerIngredients', [ BurguerController::class, 'burguerIngredients' ]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
